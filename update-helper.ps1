557bd0bacd87b88870fa94b1d8da545781dd30a1
Markson <marksonho@2333ma.top>
2022-04-08 15:15:26 +0800

    Add a Testing baulk bucket

---
+++ update-helper.ps1
@@ -0,0 +1,40 @@
+$Version_v2rayA = curl --silent "https://api.github.com/repos/v2raya/v2raya/releases/latest" | Select-String -Pattern "tag_name" | ForEach-Object { ([string]$_).Split('v')[1] } |  ForEach-Object { ([string]$_).Split('"')[0] }
+$Version_v2ray = curl --silent "https://api.github.com/repos/v2fly/v2ray-core/releases/latest" | Select-String -Pattern "tag_name" | ForEach-Object { ([string]$_).Split('v')[1] } |  ForEach-Object { ([string]$_).Split('"')[0] }
+
+$Url_v2rayA_x64 = "https://github.com/v2rayA/v2rayA/releases/download/v$Version_v2rayA/v2raya_windows_x64_$Version_v2rayA.exe"
+$Url_v2rayA_A64 = "https://github.com/v2rayA/v2rayA/releases/download/v$Version_v2rayA/v2raya_windows_arm64_$Version_v2rayA.exe"
+$Url_v2ray_x64 = "https://github.com/v2fly/v2ray-core/releases/download/v$Version_v2ray/v2ray-windows-64.zip"
+$Url_v2ray_A64 = "https://github.com/v2fly/v2ray-core/releases/download/v$Version_v2ray/v2ray-windows-arm64-v8a.zip"
+
+Write-Output $Url_v2rayA_x64 $Url_v2rayA_A64 $Url_v2ray_x64 $Url_v2ray_A64
+
+New-Item -ItemType Directory -Path "./v2rayA-x64"
+New-Item -ItemType Directory -Path "./v2rayA-x64/data"
+
+New-Item -ItemType Directory -Path "./v2rayA-A64"
+New-Item -ItemType Directory -Path "./v2rayA-A64/data"
+
+Invoke-WebRequest -Uri $Url_v2rayA_x64 -OutFile "./v2rayA-x64/v2raya.exe"
+Invoke-WebRequest -Uri $Url_v2rayA_A64 -OutFile "./v2rayA-A64/v2raya.exe"
+
+Invoke-WebRequest -Uri $Url_v2ray_x64 -OutFile "./v2ray-windows-x64.zip"
+Expand-Archive -Path "./v2ray-windows-x64.zip" -DestinationPath "./v2rayA-x64/"
+Remove-Item -Path "./v2rayA-x64/*.json" -Force -Recurse
+Move-Item -Path  "./v2rayA-x64/*.dat" -Destination "./v2rayA-x64/data" -Force
+Remove-Item -Path "./v2rayA-x64/v2ray.exe" -Force -Recurse
+
+Invoke-WebRequest -Uri $Url_v2ray_A64 -OutFile "./v2ray-windows-A64.zip"
+Expand-Archive -Path "./v2ray-windows-A64.zip" -DestinationPath "./v2rayA-A64/"
+Remove-Item -Path "./v2rayA-A64/*.json" -Force -Recurse
+Move-Item -Path  "./v2rayA-A64/*.dat" -Destination "./v2rayA-A64/data" -Force
+Remove-Item -Path "./v2rayA-A64/v2ray.exe" -Force -Recurse
+
+
+Compress-Archive -Path "./v2rayA-x64/*" -DestinationPath "./v2rayA-x64.zip"
+Compress-Archive -Path "./v2rayA-A64/*" -DestinationPath "./v2rayA-A64.zip"
+
+
+$sha256_v2rayA_x64 = Get-FileHash "./v2rayA-x64.zip" | Select-Object Hash | ForEach-Object { ([string]$_.Hash) }
+$Sha256_v2rayA_a64 = Get-FileHash "./v2rayA-A64.zip" | Select-Object Hash | ForEach-Object { ([string]$_.Hash) }
+Write-Host $sha256_v2rayA_x64 > v2rayA_x64_sha256.txt
+Write-Host $sha256_v2rayA_A64 > v2rayA_A64_sha256.txt
